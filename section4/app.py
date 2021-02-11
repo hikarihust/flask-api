c4722d273b31a153738915c589ed108cf7cc7f8c
vudinhquang <vudinhquangk53@gmail.com>
2021-02-11 20:32:41 +0900

    PUT to create or update Items

--- section4/app.py
+++ section4/app.py
@@ -32,6 +32,18 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
+    @jwt_required()
+    def put(self, name):
+        data = request.get_json()
+        # Once again, print something not in the args to verify everything works
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
 class ItemList(Resource):
     def get(self):
         return {'items': items}
