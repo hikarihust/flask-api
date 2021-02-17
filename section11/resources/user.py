9b2d8f9c5033c5ff068b63c407081cd58d5d3f9a
vudinhquang <vudinhquangk53@gmail.com>
2021-02-17 15:42:53 +0900

    A recap of the code + a few changes

--- section11/resources/user.py
+++ section11/resources/user.py
@@ -23,7 +23,7 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "User with that username already exists."}, 400
 
-        user = UserModel(data['username'], data['password'])
+        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
