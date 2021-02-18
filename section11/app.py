da8f0427153573e35d06f48c82363bcd83377114
vudinhquang <vudinhquangk53@gmail.com>
2021-02-19 03:30:45 +0900

    Customizing Flask-JWT-Extended callbacks and responses

--- section11/app.py
+++ section11/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -30,6 +30,44 @@ def add_claims_to_jwt(identity):  # Remember identity is what we define when cre
     if identity == 1:   # instead of hard-coding, we should read from a config file or database to get a list of admins instead
         return {'is_admin': True}
     return {'is_admin': False}
+
+# The following callbacks are used for customizing jwt response/error messages.
+# The original ones may not be in a very pretty format (opinionated)
+@jwt.expired_token_loader
+def expired_token_callback(jwt_headers, jwt_payload):
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):  # we have to keep the argument here, since it's passed in by the caller internally
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(jwt_headers, jwt_payload):
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader   # have been loged out
+def revoked_token_callback(jwt_headers, jwt_payload):
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
